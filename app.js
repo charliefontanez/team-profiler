432d556bf234e5af8df136fcad6a9f63ea8a10c1
Carlos Fontanez <charliefontanezis@gmail.com>
2022-05-22 15:36:42 -0400

    created containers for profile cards

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
+const inquirer = require('inquirer');
 const generatePage = require('./lib/generatePage.js');
 
 
-generatePage("Hello");
+
+// var propmtUser = () => {
+
+//   return inquirer.prompt([
+//     {
+//       tyoe: 'input'
+//     }
+//   ]);
+// }
+
+var teamData = ' '
+
+fs.writeFile("./dist/index.html", generatePage(teamData), err => {
+  if (err) {
+    reject (err);
+  }
+});
